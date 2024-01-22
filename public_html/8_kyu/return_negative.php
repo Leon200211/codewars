804ea3a605dafaaed186b3688b61b3e78a2a0cf6
Leon200211 <leon200207@yandex.ru>
2024-01-22 12:12:42 +0300

    8 kyu

---
+++ public_html/8_kyu/return_negative.php
@@ -0,0 +1,7 @@
+<?php
+
+// https://www.codewars.com/kata/55685cd7ad70877c23000102
+
+function makeNegative($num) {
+    return abs($num) * -1;
+}
---
+++ public_html/index.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once '8_kyu/return_negative.php';
+
+echo makeNegative(0);
