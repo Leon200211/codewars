321e9852547f926ee24e74d81800ef2528b1f316
Leon200211 <leon200207@yandex.ru>
2024-02-05 16:50:07 +0300

    5 kyu

--- public_html/5_kyu/merged_string_checker.php
+++ public_html/5_kyu/merged_string_checker.php
@@ -1,15 +1,46 @@
 <?php
 
+// https://www.codewars.com/kata/54c9fcad28ec4c6e680011aa/train/php
+
 function isMerge($s, $part1, $part2) {
     $sArray = str_split($s);
-    sort($sArray);
 
-    $userStr = str_split($part1 . $part2);
-    sort($userStr);
-
-    if ($part1 == 'code' && $part2 == 'wasr') {
+    if (count($sArray) !== count(str_split($part1 . $part2))) {
         return false;
     }
 
-    return (empty(array_diff($sArray, $userStr)) && count($sArray) === count($userStr)) ? true : false;
+    $part1 = str_split($part1);
+    $part2 = str_split($part2);
+
+    $result = [];
+    $arrayIter = 0;
+    while (true) {
+        if (count($result) === 0) {
+            if (!empty($part2[0]) && $part2[0] == $sArray[$arrayIter]) {
+                $part2FirstItem = array_shift($part2);
+                $result[] = $part2FirstItem;
+            } else if (!empty($part1[0]) && $part1[0] == $sArray[$arrayIter]) {
+                $part1FirstItem = array_shift($part1);
+                $result[] = $part1FirstItem;
+            } else {
+                return false;
+            }
+        } else {
+            if (!empty($part2[0]) && $part2[0] == $sArray[$arrayIter]) {
+                $part2FirstItem = array_shift($part2);
+                $result[] = $part2FirstItem;
+            } else if (!empty($part1[0]) && $part1[0] == $sArray[$arrayIter]) {
+                $part1FirstItem = array_shift($part1);
+                $result[] = $part1FirstItem;
+            } else {
+                return false;
+            }
+        }
+
+        if (count($result) === count($sArray)) {
+            return true;
+        }
+
+        $arrayIter++;
+    }
 }
--- public_html/index.php
+++ public_html/index.php
@@ -2,4 +2,4 @@
 
 require_once '5_kyu/merged_string_checker.php';
 
-var_dump(isMerge('codewars', 'code', 'warss'));
+var_dump(isMerge('Bananas from Bahamas', 'Bahas', 'Bananas from am'));
