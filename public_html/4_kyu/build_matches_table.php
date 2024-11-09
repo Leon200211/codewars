029b4ddc503249989e1a1e6928396ebab1c9e833
Leon200211 <leon200207@yandex.ru>
2024-11-09 18:18:32 +0300

    4 kyu

--- public_html/4_kyu/build_matches_table.php
+++ public_html/4_kyu/build_matches_table.php
@@ -5,6 +5,42 @@
 function buildMatchesTable(int $numberOfTeams): array
 {
     $teams = range(1, $numberOfTeams);
+
+    $away = array_splice($teams, (count($teams) / 2));
+    $home = $teams;
+    $rounds = [];
+    
+    for ($i=0; $i < count($home) + count($away) - 1; $i++) {
+        for ($j=0; $j < count($home); $j++) {
+            $rounds[$i][$j][] = $home[$j];
+            $rounds[$i][$j][] = $away[$j];
+        }
+
+        if (count($home) + count($away) - 1 > 2) {
+            $array = array_splice($home, 1, 1);
+            array_unshift($away, array_shift($array));
+            array_push($home, array_pop($away));
+        }
+    }
+
+    return $rounds;
+}
+
+function getFactorial(int $factorial): int
+{
+    $result = 1;
+
+    for($i = 1; $i <= $factorial; $i++) {
+        $result *= $i;
+    }
+
+    return $result;
+}
+
+// Не работает при большом количестве команд
+function buildMatchesTable_2(int $numberOfTeams): array
+{
+    $teams = range(1, $numberOfTeams);
     $combinationCount = getFactorial($numberOfTeams) / ((getFactorial(2) * getFactorial($numberOfTeams - 2)));
     $round = 0;
     $games = [];
@@ -60,15 +96,4 @@ function buildMatchesTable(int $numberOfTeams): array
     }
 
     return $games;
-}
-
-function getFactorial(int $factorial): int
-{
-    $result = 1;
-
-    for($i = 1; $i <= $factorial; $i++) {
-        $result *= $i;
-    }
-
-    return $result;
 }
