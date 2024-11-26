e5283124fdc05068e7daf4f894010d37b92ce3ce
Leon200211 <leon200207@yandex.ru>
2024-11-26 13:12:17 +0300

    4 kyu

--- public_html/4_kyu/dbl_linear.php
+++ public_html/4_kyu/dbl_linear.php
@@ -2,7 +2,7 @@
 
 // https://www.codewars.com/kata/5672682212c8ecf83e000050
 
-function dblLinear($n) {
+function dblLinearOptimized($n) {
     $u = [1];
     $x = 0;
     $y = 0;
@@ -31,15 +31,13 @@ function dblLinear($n) {
 
 
 
-function dblLinear_2($n) {
+function dblLinearOriginal($n) {
     $linear = [1];
     $iter = 0;
 
     while (count($linear) < $n + 4000) {
-        $firstAdd = 2 * $linear[$iter] + 1;
-        $secondAdd = 3 * $linear[$iter] + 1;
-
-
+        $linear[] = 2 * $linear[$iter] + 1;
+        $linear[] = 3 * $linear[$iter] + 1;
 
         $iter++;
 
@@ -47,8 +45,49 @@ function dblLinear_2($n) {
         sort($linear);
     }
 
-    var_dump($linear);
-    exit();
-
     return $linear[$n];
+}
+
+
+
+// Бенчмарк
+function benchmarkFunction($func, $n, $iterations = 100) {
+    $start = microtime(true);
+
+    for ($i = 0; $i < $iterations; $i++) {
+        $result = $func($n);
+    }
+
+    $end = microtime(true);
+    $time = $end - $start;
+
+    return [
+        'result' => $result,
+        'time' => $time,
+        'avg_time' => $time / $iterations
+    ];
+}
+
+// Тестовые значения
+$testValues = [10, 100, 1000, 5000];
+
+foreach ($testValues as $n) {
+    echo "Тестирование для n = $n:\n";
+
+    $originalResult = benchmarkFunction('dblLinearOriginal', $n);
+    $optimizedResult = benchmarkFunction('dblLinearOptimized', $n);
+
+    echo "Оригинальная функция:\n";
+    echo "  Результат: {$originalResult['result']}\n";
+    echo "  Общее время: " . round($originalResult['time'], 4) . " сек\n";
+    echo "  Среднее время за итерацию: " . round($originalResult['avg_time'] * 1000, 4) . " мс\n\n";
+
+    echo "Оптимизированная функция:\n";
+    echo "  Результат: {$optimizedResult['result']}\n";
+    echo "  Общее время: " . round($optimizedResult['time'], 4) . " сек\n";
+    echo "  Среднее время за итерацию: " . round($optimizedResult['avg_time'] * 1000, 4) . " мс\n\n";
+
+    $speedup = $originalResult['time'] / $optimizedResult['time'];
+    echo "Ускорение: " . round($speedup, 2) . "x\n\n";
+    echo "--------------------\n\n";
 }
