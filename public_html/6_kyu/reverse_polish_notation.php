2fc74858bcb547eb718feb42f0612ce3a36d8440
Leon200211 <leon200207@yandex.ru>
2024-12-04 16:05:33 +0300

    6 kyu

---
+++ public_html/6_kyu/reverse_polish_notation.php
@@ -0,0 +1,46 @@
+<?php
+
+// https://www.codewars.com/kata/52f78966747862fc9a0009ae
+
+function calc(string $expr): float {
+    $exprs = explode(' ', $expr);
+    $operators = ['+', '-', '*', '/'];
+    $iter = 0;
+    $result = 0;
+
+    while (!empty($exprs)) {
+        if (count($exprs) < 3) {
+            return (float)$exprs[0] ?? 0;
+        }
+
+        if (!in_array($exprs[$iter], $operators)) {
+            $iter++;
+            continue;
+        }
+
+        switch ($exprs[$iter]) {
+            case '+':
+                $result = (float)$exprs[$iter - 2] + (float)$exprs[$iter - 1];
+                break;
+            case '-':
+                $result = (float)$exprs[$iter - 2] - (float)$exprs[$iter - 1];
+                break;
+            case '*':
+                $result = (float)$exprs[$iter - 2] * (float)$exprs[$iter - 1];
+                break;
+            case '/':
+                $result = (float)$exprs[$iter - 2] / (float)$exprs[$iter - 1];
+                break;
+        }
+
+        unset($exprs[$iter - 2]);
+        unset($exprs[$iter - 1]);
+        unset($exprs[$iter]);
+        $exprs[$iter - 2] = $result;
+        ksort($exprs);
+        $exprs = array_values($exprs);
+        $iter = 0;
+    }
+
+    return $result;
+}
--- public_html/index.php
+++ public_html/index.php
@@ -1,5 +1,5 @@
 <?php
 
-require_once '7_kyu/automorphic.php';
+require_once '6_kyu/reverse_polish_notation.php';
 
-var_dump(automorphic(25));
+var_dump(calc('5 1 2 + 4 * + 3 -'));
